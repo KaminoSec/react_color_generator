ce3015d2ab3ae8bc7bf1f4d718886d3de29c4413
ozark-dev <jason@ozarkdev.com>
2023-02-28 00:01:03 -0600

    project complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js';
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#007fff').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -28,7 +28,7 @@ function App() {
             type='text'
             value={color}
             onChange={(e) => setColor(e.target.value)}
-            placeholder='#f15025'
+            placeholder='#007fff'
             className={`${error ? 'error' : null}`}
           />
           <button className='btn' type='submit'>
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,14 +5,25 @@ const SingleColor = ({ rgb, weight, index }) => {
   const [alert, setAlert] = useState(false);
   const bgc = rgb.join(',');
   const hex = rgbToHex(...rgb);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
 
   return (
     <article
       className={`color ${index > 10 && 'color-light'}`}
       style={{ backgroundColor: `rgb(${bgc})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
     >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hex}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
     </article>
   );
 };
